4ef44704f791fd68df7203db95e1c4b4dbb5bd4b
Marchingegno <matteo.marchisciana@mail.polimi.it>
2019-12-13 20:22:14 +0100

    Develop pages Not working cause bad imports

---
+++ Implementation/SafeStreetsCloud/hosting/api.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyBlIuZpimS107pjOQLOUdoScW-x63GDUw4",
+    authDomain: "safestreets-project.firebaseapp.com",
+    databaseURL: "https://safestreets-project.firebaseio.com",
+    projectId: "safestreets-project",
+    storageBucket: "safestreets-project.appspot.com",
+    messagingSenderId: "748184582401",
+    appId: "1:748184582401:web:2714dc01c3fae291248300",
+    measurementId: "G-5DGEPQJCY7"
+}
