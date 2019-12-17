8fad5249fe8a5a4435934fd3708b938230587b62
Desno365 <desno365@gmail.com>
2019-12-17 22:17:39 +0100

    Refactor and comment groupingMS

--- Implementation/SafeStreetsCloud/functions/groupingMS.js
+++ Implementation/SafeStreetsCloud/functions/groupingMS.js
@@ -10,8 +10,6 @@ const generalUtils = require('./utils/generalUtils');
 
 // Global variables
 const db = admin.firestore();
-const storage = admin.storage();
-const bucket = storage.bucket();
 
 // Constant properties
 const DISTANCE_OFFSET = 0.00001 * 5; // this is 5 meters more or less
@@ -28,7 +26,7 @@ exports.groupingMS = functions.firestore.document('/violationReports/{reportId}'
 
     if(reportStatusBefore === "SUBMITTED" && reportStatusAfter === "APPROVED") {
         console.log(`Recognized report approval.`);
-        await doThing(change.after);
+        await doGrouping(change.after);
     } else {
         console.log('Not a report approval.');
     }
@@ -36,33 +34,29 @@ exports.groupingMS = functions.firestore.document('/violationReports/{reportId}'
     return null;
 });
 
-async function doThing(violationReportSnap) {
+async function doGrouping(violationReportSnap) {
+    // Get data of the new report.
     const licensePlate = violationReportSnap.get("licensePlate");
     const latitude = violationReportSnap.get("latitude");
     const longitude = violationReportSnap.get("longitude");
     const uploadTimestamp = violationReportSnap.get("uploadTimestamp");
     const violationType = violationReportSnap.get("violationType");
+    const municipality = violationReportSnap.get("municipality");
 
-    // TODO get municipality name
-    const municipality = getMunicipalityName();
+    // Get group to which the report must be added, null if no group exists.
+    const groupDocSnap = await getGroupOfReport(municipality, licensePlate, violationType, latitude, longitude, uploadTimestamp);
 
-    const initialDate = generalUtils.getNewDateWithAddedHours(new Date(uploadTimestamp.toDate()), -TIME_OFFSET_IN_HOURS);
-    console.log("Initial date: " + initialDate.toString());
-    const finalDate = generalUtils.getNewDateWithAddedHours(new Date(uploadTimestamp.toDate()), TIME_OFFSET_IN_HOURS);
-    console.log("Final date: " + finalDate.toString());
-
-    const groupDocSnap = getGroupOfReport(municipality, licensePlate, violationType, latitude, longitude, uploadTimestamp)
-
-    if(groupDocSnap === null) {
+    if(groupDocSnap === null) { // If a group doesn't exist...
         console.log('No groups found. Creating a new group...');
-        await createNewGroup(licensePlate, violationType, initialDate, finalDate, latitude, longitude, violationReportSnap.ref.id, municipality);
+        await createNewGroup(licensePlate, violationType, uploadTimestamp, latitude, longitude, violationReportSnap.ref.id, municipality);
     } else {
         console.log('A group has been found. Adding report to the group...');
-        await addViolationReportToExistingGroup(groupDocSnap, violationReportSnap.ref.id, finalDate, municipality)
+        await addViolationReportToExistingGroup(groupDocSnap, violationReportSnap.ref.id, uploadTimestamp.toDate(), municipality)
     }
 }
 
 async function getGroupOfReport(municipality, licensePlate, violationType, latitude, longitude, uploadTimestamp) {
+    // Make query on database for getting the group (similar location and timestamp).
     // Note: Queries with range filters on different fields are not supported by Firestore.
     const querySnapshot = await db.collection("municipalities").doc(municipality).collection("groups")
         .where("licensePlate", "==", licensePlate)
@@ -71,46 +65,53 @@ async function getGroupOfReport(municipality, licensePlate, violationType, latit
         .where("latitude", "<=", latitude + DISTANCE_OFFSET)
         .get();
 
-    return getGroupDocSnapFromQuerySnapshot(querySnapshot, longitude, uploadTimestamp.toDate());
+    // Since Firestore is limited on queries we need to check separately longitude and timestamp.
+    return alsoCheckForLongitudeAndTimestampForQuery(querySnapshot, longitude, uploadTimestamp.toDate());
 }
 
-function getGroupDocSnapFromQuerySnapshot(querySnapshot, newLongitude, timestamp) {
-    for (let queryDocSnap of querySnapshot.docs) {
-        const groupLongitude = queryDocSnap.data().longitude;
-        console.log("Found group with longitude: " + groupLongitude);
-        const groupFirstTimestamp = generalUtils.getNewDateWithAddedHours(queryDocSnap.data().firstTimestamp.toDate(), -TIME_OFFSET_IN_HOURS);
-        console.log("Found group with first timestamp: " + groupFirstTimestamp.toString());
-        const groupLastTimestamp = generalUtils.getNewDateWithAddedHours(queryDocSnap.data().lastTimestamp.toDate(), TIME_OFFSET_IN_HOURS);
-        console.log("Found group with last timestamp: " + groupLastTimestamp.toString());
-        if(groupLongitude >= newLongitude - DISTANCE_OFFSET && groupLongitude <= newLongitude + DISTANCE_OFFSET && timestamp >= groupFirstTimestamp && timestamp <= groupLastTimestamp) {
-            return queryDocSnap;
+function alsoCheckForLongitudeAndTimestampForQuery(querySnapshot, newLongitude, newUploadDate) {
+    for (let groupDocSnap of querySnapshot.docs) {
+        const groupLongitude = groupDocSnap.data().longitude;
+        const groupFirstDate = groupDocSnap.data().firstTimestamp.toDate();
+        const groupLastDate = groupDocSnap.data().lastTimestamp.toDate();
+
+        // Range filter also on longitude and date.
+        if(groupLongitude >= newLongitude - DISTANCE_OFFSET && groupLongitude <= newLongitude + DISTANCE_OFFSET) {
+            if(newUploadDate >= generalUtils.getNewDateWithAddedHours(groupFirstDate, -TIME_OFFSET_IN_HOURS) && newUploadDate <= generalUtils.getNewDateWithAddedHours(groupLastDate, TIME_OFFSET_IN_HOURS))
+                return groupDocSnap; // Found the group.
         }
     }
-    return null;
+    return null; // No group has been found.
 }
 
-async function createNewGroup(licensePlate, violationType, initialDate, finalDate, latitude, longitude, violationReportId, municipality) {
+async function createNewGroup(licensePlate, violationType, uploadTimestamp, latitude, longitude, violationReportId, municipality) {
+    // Create group data.
     const newGroup = {
         licensePlate: licensePlate,
         violationType: violationType,
         groupStatus: "APPROVED",
-        firstTimestamp: initialDate,
-        lastTimestamp: finalDate,
+        firstTimestamp: uploadTimestamp,
+        lastTimestamp: uploadTimestamp,
         latitude: latitude,
         longitude: longitude,
         reports: new Array(violationReportId)
     };
+
+    // Add group to database in path: /municipalities/{municipality}/groups/{group}
     await db.collection("municipalities").doc(municipality).collection("groups").add(newGroup);
 }
 
-async function addViolationReportToExistingGroup(groupDocSnap, violationReportId, finalDate, municipality) {
+async function addViolationReportToExistingGroup(groupDocSnap, violationReportId, uploadDate, municipality) {
+    // Modify existing group data.
     const groupObject = groupDocSnap.data();
-    groupObject.reports.push(violationReportId);
-    groupObject.lastTimestamp = finalDate;
-    await db.collection("municipalities").doc(municipality).collection("groups").doc(groupDocSnap.ref.id).set(groupObject);
-}
+    const groupFirstDate = groupObject.firstTimestamp.toDate();
+    const groupLastDate = groupObject.lastTimestamp.toDate();
+    groupObject.reports.push(violationReportId); // Add report to the reports array.
+    if(uploadDate > groupLastDate) // If the new report is the last, update the lastTimestamp field.
+        groupObject.lastTimestamp = uploadDate;
+    else if(uploadDate < groupFirstDate) // If the new report is earlier than first report (processed later), update the firstTimestamp field.
+        groupObject.firstTimestamp = uploadDate;
 
-// TODO get municipality name
-function getMunicipalityName() {
-    return "testMunicip";
+    // Update data on the database.
+    await db.collection("municipalities").doc(municipality).collection("groups").doc(groupDocSnap.ref.id).set(groupObject);
 }
