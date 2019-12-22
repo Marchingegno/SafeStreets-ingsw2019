334d8913a1058d8a1965baa3e8c6b556cf1ec457
Desno365 <desno365@gmail.com>
2019-12-22 23:49:11 +0100

    Delete HelloWorld function

--- Implementation/SafeStreetsCloud/functions/index.js
+++ Implementation/SafeStreetsCloud/functions/index.js
@@ -46,14 +46,6 @@ if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'onReportStatusC
 // TODO delete
 
 /**
- * Hello world.
- */
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'helloWorld') {
-  exports.helloWorld = require('./helloWorld').helloWorldFunction;
-}
-
-
-/**
  * Triggers when a new violation report is added.
  */
 /*if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'checkViolationReports') {
