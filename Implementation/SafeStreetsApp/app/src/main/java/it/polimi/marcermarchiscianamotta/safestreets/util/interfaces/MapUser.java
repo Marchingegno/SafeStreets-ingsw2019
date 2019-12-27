dbc5182d86e7d7f290686ff38578f264693b50f0
Marcer Andrea <marcer.andrea@gmail.com>
2019-12-27 17:10:39 +0100

    Add method to MapUser for the addresses

--- Implementation/SafeStreetsApp/app/src/main/java/it/polimi/marcermarchiscianamotta/safestreets/util/interfaces/MapUser.java
+++ Implementation/SafeStreetsApp/app/src/main/java/it/polimi/marcermarchiscianamotta/safestreets/util/interfaces/MapUser.java
@@ -1,5 +1,9 @@
 package it.polimi.marcermarchiscianamotta.safestreets.util.interfaces;
 
+import android.location.Address;
+
+import androidx.annotation.Nullable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -11,5 +15,9 @@ public interface MapUser {
 	 * This method is called once the process of retrieving the current location has terminated.
 	 * @param location The location found.
 	 */
+	@Nullable
 	void onLocationFound(LatLng location);
+
+	@Nullable
+	void onAddressFound(Address address);
 }
