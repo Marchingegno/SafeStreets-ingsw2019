68d71f6431af65cafcecfbbcfa85090b0bfffe69
Marchingegno <matteo.marchisciana@mail.polimi.it>
2019-12-29 22:29:48 +0100

    Add buttons in DetailedViolationView.html Changed storage to local instead of session for ease of debugging

--- Implementation/SafeStreetsCloud/hosting/scripts/logout.js
+++ Implementation/SafeStreetsCloud/hosting/scripts/logout.js
@@ -3,6 +3,7 @@ document.getElementById("logout").addEventListener("click", logout);
 
 function logout() {
     firebase.auth().signOut().then(function () {
+        localStorage.clear();
         window.location.href = "index.html"
     }).catch(function (error) {
         console.log("Error logging out");
